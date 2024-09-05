808d08ef25a0c01fe6a3ec565b044df4dfa0998e
Italo Chiaradia <italocoelho050704@gmail.com>
2024-09-05 17:40:36 -0300

    add multer configs

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,12 +5,20 @@ const {Router} = require("express");
 const dishesRoutes = Router();
 const dishesController = new DishesController();
 
+const multer = require("multer");
+const uploadConfig = require("../configs/uploads");
+const upload = multer(uploadConfig.MULTER);
+
 dishesRoutes.post("/", ensureAuthenticated, dishesController.create);
 dishesRoutes.put("/:id", ensureAuthenticated, dishesController.update);
 dishesRoutes.delete("/:id", ensureAuthenticated, dishesController.delete);
 dishesRoutes.get("/", ensureAuthenticated, dishesController.index);
 dishesRoutes.get("/:id", ensureAuthenticated, dishesController.show);
 
+dishesRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  response.json();
+})
 
 
 module.exports = dishesRoutes;
