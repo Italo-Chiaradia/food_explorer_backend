4ccae77cdd958cf02f3e783ad220329d247bf852
Italo Chiaradia <italocoelho050704@gmail.com>
2024-09-11 14:04:43 -0300

    fix frontend address

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://foodexplorer-main.netlify.app"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://food-explorer-main.netlify.app"],
   credentials: true
 }));
 migrationsRun();
